e57f3101bd92b3f0b6305966ffaa664d8c544c64
ot904807 <ot_jiuhua.sun@mediatek.com>
2023-07-14 13:39:15 +0800

    drivers: mali-valhall: Fix for ringbuffer alloc failure
    When a fatal error occurs for a GPU queue/group, Kbase sends the error
    notification to the Userspace. As a part of error recovery, Userspace
    deletes the existing queues & groups and creates new ones.
    The fatal error is handled in the bottom half and a reference is taken
    on the GPU queue before enqueuing the work item to handle the error.
    SAME_VA zone is used for the GPU queue ringbuffer and the memory for
    ringbuffer remains pinned until the refcount of GPU queue drops to 0.
    If Userspace tries to free the ringbuffer memory whilst it is pinned,
    then only the CPU virtual range is freed but the GPU virtual range
    remains allocated, due to which CPU VA space can go out of sync with
    the GPU VA space.
    
    It could happen that Userspace, after receiving the error notification
    for one of the queues, start the error recovery actions and thereby
    delete the queues whilst the bottom half to handle the fatal error for
    the other queues is still pending/running. This can cause the creation
    of new GPU queues to fail if the same GPU VA value cant be used for
    the new queue as the CPU VA value chosen for it. Due to failure, error
    recovery wont happen and Application would crash.
    
    As a fix, this commit updates the handling of CS_QUEUE_TERMINATE ioctl
    to block the Application thread till the bottom half for handling the
    fatal error or Tiler OoM event is complete.
    
    A Base defect test has also been added to verify the creation of new
    queues do not fail after deleting the existing queues on fatal error.
    
    link: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/5470
    
    MTK-Commit-Id: 39660d96f08b62ab092d13fa182acfa5c4cd0918
    
    CR-Id: ALPS08209338
    Feature: ARM GPU DDK driver
    (cherry picked from commit 1b141605d0d6361fe0c5fe7f862a47fd88dcba55)
    
    Change-Id: If4f25f0a30051ef0fe9dd5fea201254dc42f8bd6
    Mot-CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/2678365
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jiashi Zhou <zhoujs3@motorola.com>

--- drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/csf/mali_kbase_csf.c
+++ drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/csf/mali_kbase_csf.c
@@ -725,7 +725,16 @@ void kbase_csf_queue_terminate(struct kbase_context *kctx,
 		if (!WARN_ON(!queue->queue_reg))
 			queue->queue_reg->user_data = NULL;
 		kbase_gpu_vm_unlock(kctx);
-
+		mutex_unlock(&kctx->csf.lock);
+		/* The GPU reset can be allowed now as the queue has been unbound. */
+		if (reset_prevented) {
+			kbase_reset_gpu_allow(kbdev);
+			reset_prevented = false;
+		}
+		/* The work items can be cancelled as Userspace is terminating the queue */
+		cancel_work_sync(&queue->oom_event_work);
+		cancel_work_sync(&queue->fatal_event_work);
+		mutex_lock(&kctx->csf.lock);
 		spin_lock_irqsave(&kctx->csf.event_lock, flags);
 		dev_vdbg(kctx->kbdev->dev,
 			"Remove any pending command queue fatal from context %pK\n",
@@ -2388,7 +2397,6 @@ static void oom_event_worker(struct work_struct *data)
 	mutex_lock(&kctx->csf.lock);
 
 	kbase_queue_oom_event(queue);
-	release_queue(queue);
 
 	mutex_unlock(&kctx->csf.lock);
 	kbase_reset_gpu_allow(kbdev);
@@ -2631,7 +2639,6 @@ static void fatal_event_worker(struct work_struct *const data)
 				 group_handle);
 
 unlock:
-	release_queue(queue);
 	mutex_unlock(&kctx->csf.lock);
 	if (reset_prevented)
 		kbase_reset_gpu_allow(kbdev);
@@ -2703,11 +2710,9 @@ handle_fatal_event(struct kbase_queue *const queue,
 						       RESET_FLAGS_NONE))
 				kbase_reset_gpu(queue->kctx->kbdev);
 		}
-		get_queue(queue);
 		queue->cs_fatal = cs_fatal;
 		queue->cs_fatal_info = cs_fatal_info;
-		if (!queue_work(queue->kctx->csf.wq, &queue->fatal_event_work))
-			release_queue(queue);
+		queue_work(queue->kctx->csf.wq, &queue->fatal_event_work);
 	}
 }
 
@@ -2819,7 +2824,6 @@ static void process_cs_interrupts(struct kbase_queue_group *const group,
 
 			if (((cs_req & CS_REQ_TILER_OOM_MASK) ^
 			     (cs_ack & CS_ACK_TILER_OOM_MASK))) {
-				get_queue(queue);
 				KBASE_KTRACE_ADD_CSF_GRP_Q(kbdev, CSI_TILER_OOM_INTERRUPT, group, queue,
 							   cs_req ^ cs_ack);
 				if (WARN_ON(!queue_work(wq, &queue->oom_event_work))) {
@@ -2833,7 +2837,6 @@ static void process_cs_interrupts(struct kbase_queue_group *const group,
 						kbdev->ged_log_buf_hnd_kbase, GED_LOG_ATTR_TIME,
 						"OoM event have been already queued\n");
 #endif
-					release_queue(queue);
 				}
 			}
 
