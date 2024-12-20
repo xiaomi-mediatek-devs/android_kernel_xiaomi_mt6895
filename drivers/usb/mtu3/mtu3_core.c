3254c80b7efb027230be4f09363352bd25d5c0aa
AdarshGrewal <adarshgrewal@gmail.com>
2024-12-20 12:30:38 +0530

    drivers: usb: Use cast for mtu3 speed comparisons
    Change-Id: I05d916a2765bdbe429021b44f6d80a79b19ec605

--- drivers/usb/mtu3/mtu3_core.c
+++ drivers/usb/mtu3/mtu3_core.c
@@ -715,7 +715,7 @@ static irqreturn_t mtu3_link_isr(struct mtu3 *mtu)
 	mtu->g.ep0->maxpacket = maxpkt;
 	mtu->ep0_state = MU3D_EP0_STATE_SETUP;
 
-	if (udev_speed >= MTU3_SPEED_SUPER)
+	if ((enum mtu3_speed)udev_speed >= MTU3_SPEED_SUPER)
 		ssusb_phy_dp_pullup(mtu->ssusb);
 
 	if (udev_speed == USB_SPEED_UNKNOWN)
